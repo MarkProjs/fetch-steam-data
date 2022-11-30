89036ce595b894065c8ce189922f52ee1ffd7216
Mark Agluba <mark.agluba@dawsoncollege.qc.ca>
2022-11-30 16:47:14 -0500

    commtting to the root folder and setting up the swagger

--- server/app.cjs
+++ server/app.cjs
@@ -1,10 +1,22 @@
 const express = require("express");
 const app = express();
 const api = require("./routes/api.cjs");
-
-//anything in the URL path /api uses the Router api
-app.use("/api", api);
-
+const swaggerJSDoc = require("swagger-jsdoc");
+const swaggerDefinition = {
+  openapi: '3.0.0',
+  info: {
+    title: 'Express API for JSONPlaceholder',
+    version: '1.0.0',
+  },
+};
+const options = {
+  swaggerDefinition,
+  //Paths to files containing OpenAPI definitions
+  apis: ["./routes/*.cjs"],
+}
+const swaggerSpec = swaggerJSDoc(options);
 
 //other routes - static
 app.use(express.static('../client/build'));
+
+
