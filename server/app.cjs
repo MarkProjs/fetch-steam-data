c7a3bc8fd491ae3254116a27b50424bbec4bd5ac
Mark Agluba <mark.agluba@dawsoncollege.qc.ca>
2022-11-30 15:58:35 -0500

    changing the app to be cjs instead to remove the eslint

--- server/app.cjs
+++ server/app.cjs
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const api = require("./routes/api.js");
+const api = require("./routes/api.cjs");
 
 //anything in the URL path /api uses the Router api
 app.use("/api", api);
