51a16ec1eac50ee3007a628e96f1c827b5eaee4b
Mark Agluba <mark.agluba@dawsoncollege.qc.ca>
2022-11-30 17:42:26 -0500

    started the swagger but not needed so do the default router

--- server/app.cjs
+++ server/app.cjs
@@ -1,22 +1,10 @@
 const express = require("express");
 const app = express();
 const api = require("./routes/api.cjs");
-const swaggerJSDoc = require("swagger-jsdoc");
-const swaggerDefinition = {
-  openapi: '3.0.0',
-  info: {
-    title: 'Express API for JSONPlaceholder',
-    version: '1.0.0',
-  },
-};
-const options = {
-  swaggerDefinition,
-  //Paths to files containing OpenAPI definitions
-  apis: ["./routes/*.cjs"],
-}
-const swaggerSpec = swaggerJSDoc(options);
 
+app.use('/api', api);
 //other routes - static
 app.use(express.static('../client/build'));
 
+module.exports = app
 
