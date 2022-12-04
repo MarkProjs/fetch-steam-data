5e4a6663e974741f6c8b88f229e91fcb713bc10d
BastianFC0 <bastian.fernandezcortez@dawsoncollege.qc.ca>
2022-12-04 16:50:03 -0500

    Populated totalReviews collection

--- server/util/seed.js
+++ server/util/seed.js
@@ -5,8 +5,8 @@ async function insertData(){
   try{
     const db = DB
     await db.connect("reviews", "totalReviews")
-    let insertedReviews = await db.insertMany(reviews)
-    console.log(`Inserted ${insertedReviews} reviews`)
+    let insertedReviews = await db.collection.insertMany(reviews)
+    console.log(`Inserted ${insertedReviews.insertedCount} reviews`)
   } catch(err) {
     console.error(err)
     process.exit()
