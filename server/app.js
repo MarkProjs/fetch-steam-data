699979dc1ed73f9a0171830207739282e9863193
Mark Agluba <mark.agluba@dawsoncollege.qc.ca>
2022-12-04 19:52:18 -0500

    changing the routes that bastian made to be put in the router instead

--- server/app.js
+++ server/app.js
@@ -1,53 +1,11 @@
 const express = require("express")
-const jsonFetchReviews = require("./util/jsonFetchReviews")
-const csvParse = require("./util/csvParse.js")
-const unSquashedGamesJSON = require("./data/nonfinal_data/game_genres_shortened.json")
-const getGames = require("./util/getTopAndLowGames")
-const topPositiveGames = require("./data/nonfinal_data/positiveReviews.json")
-const topNegativeGames = require("./data/nonfinal_data/negativeReviews.json")
-const getDetailedGames = require("./util/jsonFetchGameDetails")
-const insertInitialData = require("./util/seed")
 const api = require("./routes/api.cjs");
 const app = express()
+const insertInitialData = require("./util/seed")
 
 
 app.use('/api', api);
-//Utility paths
-//get the csv file data into a json Object
-app.get("/csv", async (req, res) => {
-  const jsonData = await csvParse.csvParse()
-  res.json(jsonData)
-})
 
-//Transform the unorganized json to clean data
-app.get("/organize", (req, res) => {
-  const cleanJSON = csvParse.organizeJSONArray(unSquashedGamesJSON) 
-  res.json(cleanJSON)
-})
-
-//Gets highest rated games and games with highest ratio of positive reviews/negative reviews
-app.get("/positive", (req, res) => {
-  let highestRatedGames = getGames.findHighestRatedGames()
-  res.json(highestRatedGames)
-})
-
-//Gets lowest rated games and games with highest ratio of negative reviews/positive reviews
-app.get("/negative", (req, res) => {
-  let lowestRatedGames = getGames.findLowestRatedGames()
-  res.json(lowestRatedGames)
-})
-
-//Gets details of highest rated games
-app.get("/positive-details", async (req, res) => {
-  let detailedGamesPositive = await getDetailedGames.jsonFetchPositiveGameDetails(topPositiveGames) 
-  res.json(detailedGamesPositive)
-})
-
-//Gets details of lowest rated games
-app.get("/negative-details", async (req, res) => {
-  let detailedGamesNegative = await getDetailedGames.jsonFetchNegativeGameDetails(topNegativeGames)
-  res.json(detailedGamesNegative)
-})
 
 //Populates the DB with the total review data
 app.get("/initDB", async (req, res) => {
--- server/routes/api.cjs
+++ server/routes/api.cjs
@@ -1,12 +1,31 @@
+const jsonFetchReviews = require("./util/jsonFetchReviews")
+const csvParse = require("./util/csvParse.js")
+const unSquashedGamesJSON = require("./data/nonfinal_data/game_genres_shortened.json")
+const getGames = require("./util/getTopAndLowGames")
+const topPositiveGames = require("./data/nonfinal_data/positiveReviews.json")
+const topNegativeGames = require("./data/nonfinal_data/negativeReviews.json")
+const getDetailedGames = require("./util/jsonFetchGameDetails")
 const express = require("express");
 const router = express.Router();
 
 router.use(express.json());
-
+//Utility paths
 router.get("/", (req, res)=>{
     console.log(req.body);
     res.sendStatus(201).end();
 });
+
+//get the csv file data into a json Object
+router.get("/csv", async (req, res) => {
+  const jsonData = await csvParse.csvParse()
+  res.json(jsonData)
+})
+
+//Transform the unorganized json to clean data
+router.get("/organize", (req, res) => {
+  const cleanJSON = csvParse.organizeJSONArray(unSquashedGamesJSON) 
+  res.json(cleanJSON)
+})
 
 //Gets the total positive and negative reviews of any game starting from the :number appid.
 //It loops by an arbitrary amount of times to not exceed the API request limit.
@@ -19,5 +38,31 @@ router.get("/:number", async (req, res) => {
     } catch(err) {
       console.error(err)
     }
-  })
+})
+
+//Gets highest rated games and games with highest ratio of positive reviews/negative reviews
+router.get("/positive", (req, res) => {
+  let highestRatedGames = getGames.findHighestRatedGames()
+  res.json(highestRatedGames)
+})
+
+//Gets lowest rated games and games with highest ratio of negative reviews/positive reviews
+router.get("/negative", (req, res) => {
+  let lowestRatedGames = getGames.findLowestRatedGames()
+  res.json(lowestRatedGames)
+})
+
+
+
+//Gets details of highest rated games
+router.get("/positive-details", async (req, res) => {
+  let detailedGamesPositive = await getDetailedGames.jsonFetchPositiveGameDetails(topPositiveGames) 
+  res.json(detailedGamesPositive)
+})
+
+//Gets details of lowest rated games
+router.get("/negative-details", async (req, res) => {
+  let detailedGamesNegative = await getDetailedGames.jsonFetchNegativeGameDetails(topNegativeGames)
+  res.json(detailedGamesNegative)
+})
 module.exports = router;
