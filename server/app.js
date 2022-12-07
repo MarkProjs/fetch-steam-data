ddbeb9dcbb495ae3f8cbcc508afc32d9f49a18f6
Mark Agluba <mark.agluba@dawsoncollege.qc.ca>
2022-12-07 15:54:28 -0500

    doing the routes and deleted the old one since there was a confusion

--- server/app.js
+++ server/app.js
@@ -1,19 +1,14 @@
 const express = require("express")
 const api = require("./routes/api.cjs");
 const app = express()
-const insertInitialData = require("./util/seed")
+
 
 
 app.use('/api', api);
 
 
-//Populates the DB with the total review data
-app.get("/initDB", async (req, res) => {
-  await insertInitialData()
-})
-
 //other routes - static
-app.use(express.static('../client/build'));
+app.use(express.static('public'));
 
 app.use((req, res) => {
   res.status(404).send("404 Error")
