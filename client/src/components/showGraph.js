ff6ca9b358ec1df0239ccb179384485dee2c6517
asimonelli02 <antonio.simonelli@dawsoncollege.qc.ca>
2022-12-07 16:59:48 -0500

    made simple graph

--- client/src/components/showGraph.js
+++ client/src/components/showGraph.js
@@ -2,14 +2,16 @@ import React from 'react';
 import Plot from 'react-plotly.js';
 
 function ShowGraph(){
-        return (
-            <Plot
-                data = {[
-                    {type: 'bar', x:[1,2,3], y:[2,5,3], marker: {color: 'green'}},
-                ]}
-                layout = { {width: 600, height: 600, title: 'Steam graph'} }
-            />
-        );
+  return (
+    <><Plot
+      data={[
+        {
+          x: ['RPG', 'Shooter', 'Sports', 'Adventure'],
+          y: [1, 4, 9, 16],
+          type: 'bar',
+        },
+      ]} /></>
+  );
 }
 
 export default ShowGraph;
