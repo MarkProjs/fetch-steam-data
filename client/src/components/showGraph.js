c535ebae10b7e3ecacbfdc7a301f8e85444102f4
asimonelli02 <antonio.simonelli@dawsoncollege.qc.ca>
2022-12-07 17:15:20 -0500

    decided on a graph and modified gitignore

--- client/src/components/showGraph.js
+++ client/src/components/showGraph.js
@@ -10,7 +10,18 @@ function ShowGraph(){
           y: [1, 4, 9, 16],
           type: 'bar',
         },
-      ]} /></>
+      ]}
+      // eslint-disable-next-line max-len
+      layout={{ barmode: 'relative', title: 'Steam Graph' }} /><Plot
+      data={[
+        {
+          x: ['RPG', 'Shooter', 'Sports', 'Adventure'],
+          y: [1, 4, 9, 16],
+          type: 'bar',
+        },
+      ]}
+      // eslint-disable-next-line max-len
+      layout={{ yaxis: { autorange: 'reversed'}, barmode: 'relative', title: 'Steam Graph',  }} /></>
   );
 }
 
