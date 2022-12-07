8d01b7467a691559a23305646104cb401f1b4217
BastianFC0 <bastian.fernandezcortez@dawsoncollege.qc.ca>
2022-12-07 17:18:01 -0500

    Initialized Popup

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
 import ShowGraph from './components/ShowGraph';
+import GameData from "./components/GameData"
 function App() {
   return (
     <div className="App">
       <ShowGraph></ShowGraph>
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
+
     </div>
   );
 }
