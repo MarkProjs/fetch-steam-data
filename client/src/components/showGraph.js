59a6efbf5e3ad795b4682eb38f8f5b686428d091
asimonelli02 <antonio.simonelli@dawsoncollege.qc.ca>
2022-12-09 16:38:18 -0500

    worked on the graph design

--- client/src/components/showGraph.js
+++ client/src/components/showGraph.js
@@ -3,25 +3,29 @@ import Plot from 'react-plotly.js';
 
 function ShowGraph(){
   return (
-    <><Plot
-      data={[
-        {
-          x: ['RPG', 'Shooter', 'Sports', 'Adventure'],
-          y: [1, 4, 9, 16],
-          type: 'bar',
-        },
-      ]}
-      // eslint-disable-next-line max-len
-      layout={{ barmode: 'relative', title: 'Steam Graph' }} /><Plot
-      data={[
-        {
-          x: ['RPG', 'Shooter', 'Sports', 'Adventure'],
-          y: [1, 4, 9, 16],
-          type: 'bar',
-        },
-      ]}
-      // eslint-disable-next-line max-len
-      layout={{ yaxis: { autorange: 'reversed'}, barmode: 'relative', title: 'Steam Graph',  }} /></>
+    <>
+      <div id = "plots">
+        <Plot
+          data={[
+            {
+              x: ['RPG', 'Shooter', 'Sports', 'Adventure'],
+              y: [1, 4, 9, 16],
+              type: 'bar',
+            },
+          ]}
+          // eslint-disable-next-line max-len
+          layout={{ barmode: 'relative', title: 'Steam Graph', margin: {b: 15}}} />
+        <Plot
+          data={[
+            {
+              y: [1, 4, 9, 16],
+              type: 'bar',
+            },
+          ]}
+          // eslint-disable-next-line max-len
+          layout={{ yaxis: { autorange: 'reversed'}, barmode: 'relative', margin: {t: 0}  }} />
+      </div>
+    </>
   );
 }
 
