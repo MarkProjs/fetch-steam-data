6031c9475e8506fdd2c1a079e1a9095963a25631
Antonio <antoniosimonelli28@gmail.com>
2022-12-11 00:04:46 -0500

    fetched the positive reviews

--- client/src/App.js
+++ client/src/App.js
@@ -23,6 +23,18 @@ function App() {
       }
     }
 
+    const fetchPositiveReviews = async () => {
+      try {
+        const url = "http://localhost:3001/api/positiveGamesDetails";
+        const response = await fetch(url);
+        const content = await response.json();
+        setPositiveDetials(content)
+      }
+      catch(err) {
+        console.error("Fetch Positive Details error" + err);
+      }
+    }
+
 
     fetchTotalReviews();
   }, []);
